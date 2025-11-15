5c3131f5dd01a263890eeea6ba7061be88faf33a
fthx <23138504+fthx@users.noreply.github.com>
2025-11-15 10:03:42 +0100

    Add back scroll to change profile

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@
 //    @fthx 2025 with help of @fmuellner
 
 
+import Clutter from 'gi://Clutter';
 import GObject from 'gi://GObject';
 
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
@@ -18,6 +19,8 @@ const PowerProfileIndicator = GObject.registerClass(
 
             this._setIcon();
             this.get_parent()?.connectObject('notify::allocation', () => this._setIcon(), this);
+
+            this.connectObject('scroll-event', (actor, event) => this._onScroll(event), this);
         }
 
         _setIcon() {
@@ -29,6 +32,32 @@ const PowerProfileIndicator = GObject.registerClass(
 
             this._toggle = Main.panel.statusArea.quickSettings?._powerProfiles?.quickSettingsItems[0];
             this._toggle?.bind_property('icon-name', this._indicator, 'icon-name', GObject.BindingFlags.SYNC_CREATE);
+        }
+
+        _setProfile(profile) {
+            if (this._toggle?._proxy)
+                this._toggle._proxy.ActiveProfile = profile;
+        }
+
+        _onScroll(event) {
+            const activeProfile = this._toggle?._proxy?.ActiveProfile;
+            const availableProfiles = this._toggle?._proxy?.Profiles?.map(p => p.Profile.unpack()).reverse();
+            const activeProfileIndex = availableProfiles?.indexOf(activeProfile);
+
+            let newProfile = activeProfile;
+
+            switch (event?.get_scroll_direction()) {
+                case Clutter.ScrollDirection.UP:
+                    newProfile = availableProfiles[Math.max(activeProfileIndex - 1, 0)];
+                    this._setProfile(newProfile);
+                    break;
+                case Clutter.ScrollDirection.DOWN:
+                    newProfile = availableProfiles[Math.min(activeProfileIndex + 1, availableProfiles.length - 1)];
+                    this._setProfile(newProfile);
+                    break;
+            }
+
+            return Clutter.EVENT_STOP;
         }
 
         destroy() {
